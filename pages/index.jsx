66ad6941f00fccc7753a5c742b212c1f081e63c0
Jefferson Neves <jefferson.rpn@gmail.com>
2022-01-27 09:35:21 -0300

    Ajustes gerais

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,34 +1,34 @@
 import Head from "next/head";
-import { Container, Row, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+export async function getServerSideProps() {
+  const result = await fetch(`${process.env.API_BASE_URL}/games`)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((res) => {
+      return res;
+    });
+
+  return {
+    props: {
+      games: result,
+    },
+  }
+}
 
 function Game(props) {
   const game = props.game;
   return (
     <Card>
-      <Card.Img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17e9846e348%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17e9846e348%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22106.6640625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
+      <Card.Img src={game.img} />
       <Card.Body>
         <Card.Title>{game.name}</Card.Title>
       </Card.Body>
     </Card>
-  );
-}
-
-export async function getServerSideProps() {
-  const result = await fetch(`${process.env.API_BASE_URL}/games`)
-  .then((res) => {
-    if (res.ok) {
-      return res.json()
-    }
-  })
-  .then((res) => {
-    return res;
-  });
-  
-  return {
-    props: {
-      games: result
-    }
-  }
+  )
 }
 
 export default function Home({ games }) {
@@ -42,10 +42,16 @@ export default function Home({ games }) {
       <Container>
         <h1>My Game Awards</h1>
         <p>It's just the beginning</p>
-        {games.map((game, i) => {
-          return <Game game={game}></Game>;
-        })}
+        <Row>
+          {games.map((game, i) => {
+            return (
+              <Col key={i} sm="6" lg="3">
+                <Game game={game}></Game>
+              </Col>
+            )
+          })}
+        </Row>
       </Container>
     </>
-  );
+  )
 }
